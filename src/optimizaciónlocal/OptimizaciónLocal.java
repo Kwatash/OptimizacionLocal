f82ec30867f615bd532a55de47f47dae63bfaabf
bko <bko@DESKTOP-NIMHIF7>
2022-11-22 08:47:40 -0600

    Programa 1 Optimizacion Local

---
+++ src/optimizaciónlocal/OptimizaciónLocal.java
@@ -0,0 +1,16 @@
+
+// @author omarsito
+import java.util.Scanner;
+public class OptimizaciónLocal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int suma = 0,f,valor,promedio;
+        for(f = 1; f <= 10; f++){
+                   System.out.print("Ingrese valor: ");
+                   valor = teclado.nextInt();
+                   suma = suma + valor;
+        }
+        promedio = suma / 10;
+        System.out.print("La suma es: " + suma + "El promedio es: ");
+    }  
+}
